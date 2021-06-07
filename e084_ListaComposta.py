0035501cf3fe4883d0a136fc7066670ae08a4d69
Silvia Frigatto <silvia.frigatto@gmail.com>
2021-06-07 18:17:58 -0300

    Finalizei o exercício 84.

--- e084_ListaComposta.py
+++ e084_ListaComposta.py
@@ -10,14 +10,31 @@ maior = menor = 0
 while True:
     dados.append(str(input("Nome: ")))
     dados.append(float(input("Peso: ")))
+    if len(pessoas) == 0:
+        maior = menor = dados[1]
+    else:
+        if dados[1] > maior:
+            maior = dados[1]
+        if dados[1] < menor:
+            menor = dados[1]
     pessoas.append(dados[:])
     dados.clear()
-    continuar = str(input("Continuar? [S/N]: ")).strip().upper()[0]
+    continuar = str(input("- Continuar? [S/N]: ")).strip().upper()[0]
     while continuar not in "SsNn":
-        continuar = str(input("Opção inválida. Digite S ou N: "))
+        continuar = str(input("- Opção inválida. Digite S ou N: "))
     if continuar in "Nn":
         break
-#for p in pessoas:
- #   if p[1] == 0
-print(pessoas)
 print(f"Total de pessoas cadastradas: {len(pessoas)}")
+print(f"Maior peso: {maior}kg")
+for lista in pessoas:
+    if lista[1] == maior:
+        print(f"- {lista[0]}")
+print(f"Menor peso: {menor}kg")
+for lista in pessoas:
+    if lista[1] == menor:
+        print(f"- {lista[0]}")
+
+
+
+
+
