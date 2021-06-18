caf049a36ed305733d566a5b4ea5ace20b0a6a28
Silvia Frigatto <silvia.frigatto@gmail.com>
2021-06-18 18:36:25 -0300

    Finalizei o exercício 99.

---
+++ e099_FuncaoMaior.py
@@ -0,0 +1,35 @@
+# Faça um programa que tenha uma função chamada maior(), que receba vários
+# parâmetros com valores inteiros. Seu programa tem que analisar todos os
+# valores e dizer qual deles é o maior.
+
+
+from time import sleep
+
+
+def maior(*num):
+    print("*-" * 30)
+    print(f"Os valores informados foram:", end=" ")
+    lista = list(num)
+    for valor in lista:
+        sleep(.5)
+        print(f"{valor}", end=" ")
+    sleep(.5)
+    if len(lista) == 0:
+        num = 0
+        print(f"\nTotal de números informados: {num}")
+        print(f"O maior valor informado foi: {num}")
+    else:
+        print(f"\nTotal de números informados: {len(lista)}")
+        print(f"O maior valor informado foi: {max(lista)}")
+    sleep(2)
+
+
+maior(1, 2, 3)
+maior(1, 5, 7, 4)
+maior(5, 1, 25, 10, 2)
+maior(-10, -20)
+maior()
+
+
+
+
